e05b00393b86ae87b775123cea9fdc1c551b64a3
Rishi-kumar18 <rk18102004@gmail.com>
2024-08-29 15:53:55 +0530

    graphs

--- Frontend/YoYo/src/pages/Home.jsx
+++ Frontend/YoYo/src/pages/Home.jsx
@@ -54,7 +54,7 @@ function Home() {
           </TabsContent>
         </Tabs>
         <DownloadJson data={jsonData} />
-        <DataVisualization />
+        <DataVisualization jsondata={jsonData} />
       </div>
     </>
   );
